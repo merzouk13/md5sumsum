64913a66bb0e816f67204127cff6fd415aae6a4e
merzouk <merzoukoumedda@gmail.com>
2020-05-31 16:51:19 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::{env, process::Command};
 
 fn main() {
     let mut paths: Vec<String> = vec![];
+    let mut i =0;
+    let mut f;
+    let mut hashcat = String::from("");
     for arg in env::args().skip(1) {
         for entry in walkdir::WalkDir::new(arg)
             .into_iter()
@@ -11,15 +14,16 @@ fn main() {
         {
             paths.push(String::from(entry.path().to_string_lossy()))
         }
-    }
-    let mut hashcat = String::from("");
-    for f in paths {
+        f = paths[i].clone();
         let mut cmd = Command::new("sh");
         cmd.arg("-c").arg(format!("md5sum \"{}\"", f));
         let output = String::from_utf8_lossy(&cmd.output().unwrap().stdout).to_string();
         let vec: Vec<&str> = output.split(" ").collect();
         hashcat += vec[0];
+        i =i+1;
     }
+    
+  
     let mut cmd = Command::new("sh");
     cmd.arg("-c").arg(format!("echo \"{}\" | md5sum", hashcat));
     let output = String::from_utf8_lossy(&cmd.output().unwrap().stdout).to_string();
